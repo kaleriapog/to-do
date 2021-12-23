442820fc6b4dae1517d63673d11eee374e0ceeff
pogrelova.kaleria <pogrelova.kaleria@gmail.com>
2021-12-23 12:22:29 +0200

    added the ability to register and login + favicon, but still without error handling

--- js/API.js
+++ js/API.js
@@ -1,50 +1,33 @@
 let formReg = document.forms.regform;
-let nameReg = '';
-let emailReg = '';
-let ageReg = '';
-let passwordReg = '';
+let nameReg;
+let emailReg;
+let ageReg;
+let passwordReg;
+let formSignin = document.forms.signinform;
+let emailSignin;
+let passworSignin;
 
 // for api
 var myHeaders = new Headers();
 myHeaders.append("Content-Type", "application/json");
 
-// var raw = JSON.stringify({
-//   "name": a,
-//   "email": b,
-//   "password": "testtesttest2",
-//   "age": 152
-// });
-
-// var requestOptions = {
-//   method: 'POST',
-//   headers: myHeaders,
-//   body: raw,
-//   redirect: 'follow'
-// };
-
-// fetch("https://api-nodejs-todolist.herokuapp.com/user/register", requestOptions)
-//   .then(response => response.text())
-//   .then(result => console.log(result))
-//   .catch(error => console.log('error', error));
-
-  
 // форма получение данных
 
-if(formReg !== null) {
+if(formReg !== undefined) {
     document.forms.regform.onsubmit = function() {
         nameReg = this.namereg.value;
         emailReg = this.emailreg.value;
         ageReg = this.agereg.value;
         passwordReg = this.passwordreg.value;
 
-        var raw = JSON.stringify({
+        let raw = JSON.stringify({
             "name": nameReg,
             "email": emailReg,
             "password": passwordReg,
             "age": ageReg
         });
 
-        var requestOptions = {
+        let requestOptions = {
             method: 'POST',
             headers: myHeaders,
             body: raw,
@@ -55,12 +38,37 @@ if(formReg !== null) {
         .then(response => response.text())
         .then(result => console.log(result))
         .catch(error => console.log('error', error));
-        
-        console.log(nameReg)
-        console.log(emailReg)
-        console.log(ageReg)
-        console.log(passwordReg)
 
         return false;
     };
+}
+
+// логинизация
+
+if(formSignin !== undefined) {
+
+    document.forms.signinform.onsubmit = function() {
+        
+        emailSignin = this.emailsignin.value;
+        passworSignin = this.passwordsignin.value;
+
+        let raw = JSON.stringify({
+        "email": emailSignin,
+        "password": passworSignin
+        });
+
+        let requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+        };
+
+        fetch("https://api-nodejs-todolist.herokuapp.com/user/login", requestOptions)
+        .then(response => response.text())
+        .then(result => document.location.href = 'to-do-list.html')
+        .catch(error => console.log('error', error));
+
+        return false;
+    }
 }
